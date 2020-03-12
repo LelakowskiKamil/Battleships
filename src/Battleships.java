db2426941d93b72bb8f83afd8489969dcce85089
Kamil Lelakowski <kamilel1997@gmail.com>
2020-03-12 23:49:17 +0100

    add ComputerShipGenerator
    to do:
    change void board to string board>>

--- src/Battleships.java
+++ src/Battleships.java
@@ -1,15 +1,18 @@
 import java.sql.SQLOutput;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Battleships {
     public static void main(String[] args) {
         Board myBoard = new Board("X");
-
-      //  generateBoard(myBoard.board);
-        generateEmptyBoard(myBoard.board);
-        printBoard(myBoard.board);
-        setShip(myBoard.board,myBoard.marker);
-
+        Board computerBoard = new Board("@");
+       // generateEmptyBoard(myBoard.board);
+       // printBoard(myBoard.board);
+       // setShip(myBoard.board,myBoard.marker);
+        generateEmptyBoard(computerBoard.board);
+        computerSetShip(computerBoard.board, computerBoard.marker);
+       // computerBoard.board=computerSetShip(computerBoard.board, computerBoard.marker);
+//printBoard(computerBoard.board);
 
     }
     static void printBoard(String[][] board){
@@ -214,4 +217,316 @@ shipSize++;
 
     }
     }
+    public static String[][] computerSetShip(String[][] board, String marker){
+        int count=0;
+        Random random = new Random();
+        boolean tic = false;
+        int shipSize=5;
+        while(shipSize>1){
+            Ship ship = new Ship(shipSize);
+            while (ship.numberOfShipToBeStacked > 0) {
+            try {
+                count++;
+                System.out.println("counter = "+count);
+                System.out.println("rozmiar kolcka: "+shipSize+" zostalo: "+ship.numberOfShipToBeStacked);
+                int row = random.nextInt(11) + 1;
+                int col = random.nextInt(11) + 1;
+
+                if(shipSize==5){
+                    if (count==35){
+                        break;
+                    }
+                    if(col<2 || col>7){
+                        continue;}
+                    else {
+                        if(board[row][col].equals(marker) ||
+                                board[row][col - 1].equals(marker) ||
+                                board[row][col + 1].equals(marker) ||
+                                board[row][col + 2].equals(marker) ||
+                                board[row][col + 3].equals(marker)){
+                            continue;}
+                        else{
+                            if (board[row - 1][col - 2].equals(marker) ||
+                                    board[row - 1][col - 1].equals(marker) ||
+                                    board[row - 1][col].equals(marker) ||
+                                    board[row - 1][col + 1].equals(marker) ||
+                                    board[row - 1][col + 2].equals(marker) ||
+                                    board[row - 1][col + 3].equals(marker) ||
+                                    board[row - 1][col + 4].equals(marker) ||
+
+                                    board[row + 1][col - 2].equals(marker) ||
+                                    board[row + 1][col - 1].equals(marker) ||
+                                    board[row + 1][col].equals(marker) ||
+                                    board[row + 1][col + 1].equals(marker) ||
+                                    board[row + 1][col + 2].equals(marker) ||
+                                    board[row + 1][col + 3].equals(marker) ||
+                                    board[row + 1][col + 4].equals(marker) ||
+
+                                    board[row][col - 2].equals(marker) ||
+                                    board[row][col + 4].equals(marker)) {
+                                continue;}
+                            else {
+                                board[row][col] = marker;
+                                board[row][col - 1] = marker;
+                                board[row][col + 1] = marker;
+                                board[row][col + 2] = marker;
+                                board[row][col + 3] = marker;
+                                ship.numberOfShipToBeStacked--;
+                                count=0;
+                            }
+                        }
+                    }
 }
+                else if(shipSize==4){
+                    if (count==100){
+                        break;
+                    }
+                    if (tic){
+                        tic = !tic;
+                        if(row<2 || row>8){
+                            continue;
+                        } else {
+                            if(board[row][col].equals(marker) ||
+                                    board[row+1][col].equals(marker) ||
+                                    board[row-1][col].equals(marker) ||
+                                    board[row-2][col].equals(marker) ){
+                                continue;
+                            }else{
+                                if (board[row-2][col-1].equals(marker) ||
+                                        board[row-1][col-1].equals(marker) ||
+                                        board[row][col-1].equals(marker) ||
+                                        board[row+1][col-1].equals(marker) ||
+                                        board[row+2][col-1].equals(marker) ||
+                                        board[row+3][col-1].equals(marker) ||
+
+                                        board[row - 2][col+ 1].equals(marker) ||
+                                        board[row - 1][col + 1].equals(marker) ||
+                                        board[row][col + 1].equals(marker) ||
+                                        board[row + 1][col + 1].equals(marker) ||
+                                        board[row+2][col + 1].equals(marker) ||
+                                        board[row+3][col + 1].equals(marker) ||
+
+                                        board[row-2][col].equals(marker) ||
+                                        board[row+3][col].equals(marker)) {
+                                    continue;
+                                } else {
+                                    board[row+2][col] = marker;
+                                    board[row+1][col] = marker;
+                                    board[row][col] = marker;
+                                    board[row-1][col] = marker;
+                                    ship.numberOfShipToBeStacked--;
+                                    count=0;
+                                    if (ship.numberOfShipToBeStacked==0){
+                                        continue;}
+                                }
+                            }
+
+                        }
+                    }
+                    else{
+                        tic = !tic;
+    if(col<2 || col>8){
+        continue;}
+    else {
+        if(board[row][col].equals(marker) ||
+                board[row][col - 1].equals(marker) ||
+                board[row][col + 1].equals(marker) ||
+                board[row][col + 2].equals(marker)){
+            continue;}
+        else{
+            if (board[row - 1][col - 2].equals(marker) ||
+                    board[row - 1][col - 1].equals(marker) ||
+                    board[row - 1][col].equals(marker) ||
+                    board[row - 1][col + 1].equals(marker) ||
+                    board[row - 1][col + 2].equals(marker) ||
+                    board[row - 1][col + 3].equals(marker) ||
+
+                    board[row + 1][col - 2].equals(marker) ||
+                    board[row + 1][col - 1].equals(marker) ||
+                    board[row + 1][col].equals(marker) ||
+                    board[row + 1][col + 1].equals(marker) ||
+                    board[row + 1][col + 2].equals(marker) ||
+                    board[row + 1][col + 3].equals(marker) ||
+
+                    board[row][col - 2].equals(marker) ||
+                    board[row][col + 3].equals(marker)) {
+                continue;
+            } else {
+                board[row][col] = marker;
+                board[row][col - 1] = marker;
+                board[row][col + 1] = marker;
+                board[row][col + 2] = marker;
+                ship.numberOfShipToBeStacked--;
+                count=0;
+
+                if (ship.numberOfShipToBeStacked==0){
+                    continue;}
+            }
+        }
+    }}
+}
+                else if(shipSize==3){
+                    if (count==100){
+                        break;
+                    }
+                    if (tic){
+                        tic = !tic;
+                        if(row<2 || row>9){
+                            continue;
+                        } else {
+                            if(board[row][col].equals(marker) ||
+                                    board[row+1][col].equals(marker) ||
+                                    board[row-1][col].equals(marker) ){
+                                continue;
+                            }else{
+                                if (board[row-2][col-1].equals(marker) ||
+                                        board[row-1][col-1].equals(marker) ||
+                                        board[row][col-1].equals(marker) ||
+                                        board[row+1][col-1].equals(marker) ||
+                                        board[row+2][col-1].equals(marker) ||
+
+                                        board[row - 2][col+ 1].equals(marker) ||
+                                        board[row - 1][col + 1].equals(marker) ||
+                                        board[row][col + 1].equals(marker) ||
+                                        board[row + 1][col + 1].equals(marker) ||
+                                        board[row+2][col + 1].equals(marker) ||
+
+                                        board[row-2][col].equals(marker) ||
+                                        board[row+2][col].equals(marker)) {
+                                    continue;
+                                } else {
+                                    board[row+1][col] = marker;
+                                    board[row][col] = marker;
+                                    board[row-1][col] = marker;
+                                    ship.numberOfShipToBeStacked--;
+                                    count=0;
+                                    if (ship.numberOfShipToBeStacked==0){
+                                        continue;}
+                                }
+                            }
+                        }
+                    }
+                    else{
+                        tic = !tic;
+                        if(col<2 || col>9){
+                            continue;}
+                        else {
+                            if(board[row][col].equals(marker) ||
+                                    board[row][col - 1].equals(marker) ||
+                                    board[row][col + 1].equals(marker)){
+                                continue;}
+                            else{
+                                if (board[row - 1][col - 2].equals(marker) ||
+                                        board[row - 1][col - 1].equals(marker) ||
+                                        board[row - 1][col].equals(marker) ||
+                                        board[row - 1][col + 1].equals(marker) ||
+                                        board[row - 1][col + 2].equals(marker) ||
+
+                                        board[row + 1][col - 2].equals(marker) ||
+                                        board[row + 1][col - 1].equals(marker) ||
+                                        board[row + 1][col].equals(marker) ||
+                                        board[row + 1][col + 1].equals(marker) ||
+                                        board[row + 1][col + 2].equals(marker) ||
+
+                                        board[row][col - 2].equals(marker) ||
+                                        board[row][col + 2].equals(marker)) {
+                                    continue;
+                                } else {
+                                    board[row][col] = marker;
+                                    board[row][col - 1] = marker;
+                                    board[row][col + 1] = marker;
+                                    ship.numberOfShipToBeStacked--;
+                                    count=0;
+                                    if (ship.numberOfShipToBeStacked==0){
+                                        continue;}}
+                            }}}
+                    }
+                else if(shipSize==2) {
+                    if (count==100){
+                        break;
+                    }
+                    if (tic){
+                        tic = !tic;
+                        if(row<1 || row>9){
+                            continue;
+                        } else {
+                            if(board[row][col].equals(marker) ||
+                                    board[row+1][col].equals(marker)){
+                                continue;
+                            }else{
+                                if (    board[row-1][col-1].equals(marker) ||
+                                        board[row][col-1].equals(marker) ||
+                                        board[row+1][col-1].equals(marker) ||
+                                        board[row+2][col-1].equals(marker) ||
+
+                                        board[row - 1][col + 1].equals(marker) ||
+                                        board[row][col + 1].equals(marker) ||
+                                        board[row + 1][col + 1].equals(marker) ||
+                                        board[row+2][col + 1].equals(marker) ||
+
+                                        board[row-1][col].equals(marker) ||
+                                        board[row+2][col].equals(marker)) {
+                                    continue;
+                                } else {
+                                    board[row+1][col] = marker;
+                                    board[row][col] = marker;
+                                    ship.numberOfShipToBeStacked--;
+                                    count=0;
+                                    if (ship.numberOfShipToBeStacked==0){
+                                        continue;}
+                                }
+                            }
+                    }}
+                    else{
+    tic= !tic;
+    if (col < 1 || col > 9) {
+        continue;
+    } else {
+        if (board[row][col].equals(marker) ||
+                board[row][col + 1].equals(marker)) {
+            continue;
+        } else {
+            if (board[row - 1][col - 1].equals(marker) ||
+                    board[row - 1][col].equals(marker) ||
+                    board[row - 1][col + 1].equals(marker) ||
+                    board[row - 1][col + 2].equals(marker) ||
+
+                    board[row + 1][col - 1].equals(marker) ||
+                    board[row + 1][col].equals(marker) ||
+                    board[row + 1][col + 1].equals(marker) ||
+                    board[row + 1][col + 2].equals(marker) ||
+
+                    board[row][col - 1].equals(marker) ||
+                    board[row][col + 2].equals(marker)) {
+                continue;
+            } else {
+                board[row][col] = marker;
+                board[row][col + 1] = marker;
+                ship.numberOfShipToBeStacked--;
+                count=0;
+                if (ship.numberOfShipToBeStacked==0){
+                    continue;}
+            }
+        }
+    }}
+}
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e.toString());
+            }
+
+    }
+            if (count==100){
+                break;
+            }
+            shipSize--;
+            printBoard(board);
+}
+        if (count==100){
+            generateEmptyBoard(board);
+            computerSetShip(board,marker);
+        }
+        return board;
+}
+}
+
